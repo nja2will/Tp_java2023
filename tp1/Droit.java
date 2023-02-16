29ee581af0672d35796162e25f54459d344d37d6
nja2will <wilfriednjanja4@gmail.com>
2023-02-16 12:52:14 +0100

    droit good

--- tp1/Droit.java
+++ tp1/Droit.java
@@ -1,10 +1,13 @@
 public class Droit {
     public static void main(String[] args){
         System.out.println(appartient(1,0,1,1,0));
+        droit(1,2,5,6);
     }
     public static void droit(double x1,double y1,double x2,double y2){
-
-
+        double b = -(x2-x1);
+        double a = y2-y1;
+        double c = a*x1+b*x2;
+        System.out.println("a= "+a+" , b =  "+b+" et c = "+c);
     }
     public static boolean appartient (double a,double b,double c,double x,double y){
         if (c==(a*x+b*y)){
